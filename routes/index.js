05c85b84e75208b5399404159c453045d193a259
Tran Xuan Son <tranxuanson2013@gmail.com>
2021-11-14 02:35:48 +0700

    sign in

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/sign-in', (req, res, next) => {
+  res.render('signIn');
+})
+
 module.exports = router;
