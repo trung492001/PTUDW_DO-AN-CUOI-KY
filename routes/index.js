c023e26b1510f376b124ccaa5fba5b3323f06a26
TRUNG <dotientrung201030@gmail.com>
2021-11-23 16:49:29 +0700

    add images

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 const dishModels = require('../models/dish.model');
 /* GET home page. */
-<<<<<<< HEAD
 router.get('/', function(req, res, next) {
   res.render('index');
-=======
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
->>>>>>> 2e844d551cc4f4e6b6d9c4ccd1f91a72ce0958f2
 });
 
 router.get('/sign-in', (req, res, next) => {
