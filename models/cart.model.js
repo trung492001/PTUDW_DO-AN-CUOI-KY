72ec6c562154a22b611159868cdc54d952801006
Tran Xuan Son <tranxuanson2013@gmail.com>
2021-11-24 20:35:59 +0700

    update model

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose');
 
 const cartModels = new mongoose.Schema({
-    userId: String,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'customer'
+    },
     cart: [
         {
-            dishId: String,
-            Qty: String,
-            price: String,
+            bookId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'dish'
+            },
+            quantity: {
+                type: Number,
+                min: 1,
+                max: 10
+            },
+            unitPrice: {
+                type: Number,
+                min: 0
+            }
         },
     ],
+    shippingFee: {
+        type: Number,
+        min: 0
+    },
     address: String,
     phone: String,
+    status: Number,
     total: String,
 });
 
