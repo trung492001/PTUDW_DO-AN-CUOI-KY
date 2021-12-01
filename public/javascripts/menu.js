b0032bbb3fde24872436fd4af3ad4cdd8ca1699c
Tran Xuan Son <tranxuanson2013@gmail.com>
2021-12-01 23:10:21 +0700

    add price

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,9 +1,19 @@
-function openModalAddDish(){
-    document.getElementById('dish-form').reset();
+function openModalAddDish() {
+    const formDish = document.getElementById('dish-form');
+    formDish.reset();
+    formDish.setAttribute("action", "/dish");
+    formDish.setAttribute("method", "POST");
     document.getElementById('dish-modal').classList.remove('hidden');
     document.getElementById('modal-label').innerText = "Add dish";
 }
 
-function closeModalDish(){
+function openModalEditDish(dishId, dishName, dishPrice,) {
+    const formDish = document.getElementById('dish-form');
+    formDish.reset();
+    formDish.setAttribute("action", `/dish/${dishId}`);
+    formDish.setAttribute("method", "PATCH");
+}
+
+function closeModalDish() {
     document.getElementById('dish-modal').classList.add('hidden');
 }
