ae26c2b131b7361798d6093bc4e0c47ff7253ca1
TRUNG <dotientrung201030@gmail.com>
2021-12-05 13:34:03 +0700

    fix dropdown and avata

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,7 +1,6 @@
-console.log("LOL");
 const btn = document.querySelector('button.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
 
 btn.addEventListener('click', () => {
     menu.classList.toggle("hidden");
-})
+});
