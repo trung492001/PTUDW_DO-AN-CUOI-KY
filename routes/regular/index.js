028b183b0ebdd7202d5bae052f6a500960f9bd86
TRUNG <dotientrung201030@gmail.com>
2021-12-12 18:47:04 +0700

    footer and laptop.pug_v1

--- routes/regular/index.js
+++ routes/regular/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const authRoute = require('./auth.route');
 const indexController = require('../../controllers/auth/index.controller');
+const laptopController = require('../../controllers/auth/laptop.controller');
 
 router.use('/', authRoute);
 
 router.get('/', indexController);
+
+router.get('/laptop', laptopController);
 
 router.get('/404', (req, res) => res.render('404'));
 
