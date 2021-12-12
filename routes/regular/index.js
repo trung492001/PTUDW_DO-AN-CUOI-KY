15a7806fa916386d712bf3d698fe0913d77e0202
tvqphuoc01 <tvqphuoc.01@gmail.com>
2021-12-12 23:24:37 +0700

    productDetail - BE

--- routes/regular/index.js
+++ routes/regular/index.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 const authRoute = require('./auth.route');
 const indexController = require('../../controllers/auth/index.controller');
+const laptopController = require('../../controllers/page/laptop.controller');
+const detailLaptopController = require('../../controllers/page/productDetail.controller');
 
 router.use('/', authRoute);
 
 router.get('/', indexController);
 
-router.get('/laptop', (req, res) => {
-  res.render('laptop');
-})
+router.get('/laptop', laptopController);
+
+router.get('/detailLaptop?', detailLaptopController);
 
 router.get('/404', (req, res) => res.render('404'));
 
--- service/productData.service.js
+++ service/productData.service.js
@@ -1,18 +1,29 @@
 const productModels = require('../models/product.model');
 
+// Lay toan bo du lieu ve Product
 const getProductData = async function() {
     const productData = await productModels.find();
     return productData;
 };
 
-const getRandomProduct = async function() {
+// Lay Random 10 products
+const getRandomProduct = async function(size) {
     const productData = await productModels.aggregate([
-        { $sample: { size: 10 } }
+        { $sample: { size: size } }
     ]);
+    return productData;
+};
+
+// Lay 1 product theo Id
+const getOneProduct = async function(productId) {
+    const productData = await productModels.findOne(
+        {_id: productId}
+    );
     return productData;
 };
 
 module.exports = {
     getProductData,
-    getRandomProduct
+    getRandomProduct,
+    getOneProduct
 }
