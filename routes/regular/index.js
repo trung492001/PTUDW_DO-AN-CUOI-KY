d4bd84918f875fe592663644a1e82e0b9b1867cc
TRUNG <dotientrung201030@gmail.com>
2021-12-12 20:50:19 +0700

    laptop.pug v2

--- routes/regular/index.js
+++ routes/regular/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const authRoute = require('./auth.route');
-const indexController = require('../../controllers/auth/index.controller');
-const laptopController = require('../../controllers/auth/laptop.controller');
 
 router.use('/', authRoute);
 
