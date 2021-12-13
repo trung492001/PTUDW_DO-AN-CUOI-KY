52a6dc04c22a8a61837d4303ddcc3e3eadba0efd
Tran Xuan Son <tranxuanson2013@gmail.com>
2021-12-14 06:05:39 +0700

    add comment UI

---
+++ public/javascripts/toast.js
@@ -0,0 +1,13 @@
+const Toast = {
+  alert: (text) => Toastify({
+    text: text,
+    duration: 3000,
+    close: true,
+    gravity: "top", 
+    position: "right",
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style:{
+      background: "red"
+    }
+  }).showToast()
+}
