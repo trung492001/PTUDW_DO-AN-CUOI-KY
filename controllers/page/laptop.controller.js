ff6e0c9f5b7b4d8fdb1e726e1fa8954f48b4fd8a
tvqphuoc01 <tvqphuoc.01@gmail.com>
2021-12-15 00:43:41 +0700

    fix merge

--- controllers/page/laptop.controller.js
+++ controllers/page/laptop.controller.js
@@ -7,8 +7,38 @@ module.exports = async (req, res) => {
     const productPerPage = 20;
     const begin = (page - 1) * productPerPage;
     const end = page * productPerPage;
-    const productArray = productData.slice(begin, end);
-    res.locals.productData = JSON.stringify(productData);
+
+    if(req.query.searchId) {
+        let searchOption = req.query.searchId.replaceAll('list','').split('_');
+        let typeOption = req.query.typeId.split('_');
+        console.log(searchOption);
+        productData = await productService.getProductData();
+        for(let i = 0; i < searchOption.length; i++) {
+            if(searchOption[i] == '1') {
+                for(let j = 0; j < productData.length; j++) {
+                    if(productData[j].brand === parseInt(typeOption[i])) {
+                        data.push(productData[j]);
+                    }
+                }
+            }
+            if(searchOption[i] == '2') {
+                if(data === []) {
+                    data = await productService.getProductData();
+                    data = productService.filterPrice(data, parseInt(typeOption[i]));
+                } else {
+                    data = productService.filterPrice(data, parseInt(typeOption[i]));
+                }
+            }
+        }
+        currentUrl = req.originalUrl.replace('/laptop','').replace('?','');
+        currentUrl = currentUrl.replace('&page='+page,'');
+        productArray = data.slice(begin, end);
+    } else {
+        productData = await productService.getProductData();
+        currentUrl = '';
+        productArray = productData.slice(begin, end);
+    }
+
     res.locals.currentPage = parseInt(page);
     res.locals.product = productArray;
     res.render('laptop');
