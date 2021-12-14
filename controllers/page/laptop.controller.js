d05e8a392f2963049e945d5758563920d515b95f
tvqphuoc01 <tvqphuoc.01@gmail.com>
2021-12-14 20:09:10 +0700

    add filter by brand and price

--- controllers/page/laptop.controller.js
+++ controllers/page/laptop.controller.js
@@ -1,15 +1,49 @@
 const productService = require('../../service/productData.service');
 
 module.exports = async (req, res) => {
-    const productData = await productService.getProductData();
+    let productData = [];
+    let currentUrl;
+    let data = [];
+    let productArray = [];
     // Pagination
     const page = parseInt(req.query.page) || 1;
     const productPerPage = 20;
     const begin = (page - 1) * productPerPage;
     const end = page * productPerPage;
-    const productArray = productData.slice(begin, end);
-    res.locals.productData = JSON.stringify(productData);
+
+    if(req.query.searchId) {
+        let searchOption = req.query.searchId.replaceAll('list','').split('_');
+        let typeOption = req.query.typeId.split('_');
+        console.log(searchOption);
+        productData = await productService.getProductData();
+        for(let i = 0; i < searchOption.length; i++) {
+            if(searchOption[i] == '1') {
+                for(let j = 0; j < productData.length; j++) {
+                    if(productData[j].brand === parseInt(typeOption[i])) {
+                        data.push(productData[j]);
+                    }
+                }
+            }
+            if(searchOption[i] == '2') {
+                if(data === []) {
+                    data = await productService.getProductData();
+                    data = productService.filterPrice(data, parseInt(typeOption[i]));
+                } else {
+                    data = productService.filterPrice(data, parseInt(typeOption[i]));
+                }
+            }
+        }
+        currentUrl = req.originalUrl.replace('/laptop','').replace('?','');
+        currentUrl = currentUrl.replace('&page='+page,'');
+        productArray = data.slice(begin, end);
+    } else {
+        productData = await productService.getProductData();
+        currentUrl = '';
+        productArray = productData.slice(begin, end);
+    }
+
     res.locals.currentPage = parseInt(page);
     res.locals.product = productArray;
+    res.locals.currentUrl = currentUrl;
     res.render('laptop');
 }
--- routes/regular/index.js
+++ routes/regular/index.js
@@ -5,7 +5,6 @@ const authRoute = require('./auth.route');
 const indexController = require('../../controllers/auth/index.controller');
 const laptopController = require('../../controllers/page/laptop.controller');
 const detailLaptopController = require('../../controllers/page/productDetail.controller');
-const filterController = require('../../controllers/page/filter.controller');
 
 router.use('/', authRoute);
 
@@ -20,7 +19,5 @@ router.get('/laptop/:id', (req, res) => res.render('detailLaptop'));
 router.get('/404', (req, res) => res.render('404'));
 
 router.get('/ShoppingCart', (req, res) => res.render('cart'));
-
-router.post('/filter', filterController);
 
 module.exports = router;
